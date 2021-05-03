8ea27e28143a57a188d26c207ca9cdcdb2a5ac8d
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2021-05-03 08:44:55 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# Getting started with pyodide in React
+
+This repository shows how to classify Iris using scikit-learn directly inside your react app (all happening client-side).
+
+![Image demoing the Iris app](./images/iris-demo.gif)
+
+The app will take one minute to load the first time since it is downloading large files through the network.
+
+## Quickstart
+
+npm install node-modules : Installing all the essential unit of logic for a specific functionality or a full-fledged library, such as reactstrap, material-ui, redux-form, and so on.
+
+npm start- runs our server file
+
+## Acknowledgement
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+
+To learn React, check out the [React documentation](https://reactjs.org/).
---
+++ src/App.js
@@ -0,0 +1,125 @@
+import { useEffect, useState, useRef } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import github from './images/GitHub.png';
+
+import trainScript from './python/train.py';
+import predScript from './python/predict.py';
+
+const urls = {
+  virginica: 'https://upload.wikimedia.org/wikipedia/commons/9/9f/Iris_virginica.jpg',
+  versicolor: 'https://upload.wikimedia.org/wikipedia/commons/a/ab/Iris_versicolor_Quebec_2.jpg',
+  setosa: 'https://upload.wikimedia.org/wikipedia/commons/5/56/Kosaciec_szczecinkowaty_Iris_setosa.jpg'
+}
+
+const round = (n) => Number.parseFloat(n).toFixed(2);
+
+const runModule = (script, func) => {
+  return fetch(script).then(src => src.text()).then(func)
+}
+
+const loadModule = async script => {
+  const code = await fetch(script).then(src => src.text());
+  return code;
+}
+
+
+const AnnotatedSlider = props => {
+  const [value, setValue] = useState(round(props.value));
+
+  return (
+    <div>
+      <div className="App-slider-desc">
+        {props.name}: {props.value} cm
+      </div>
+      <div className="App-slider">
+        <input
+          type="range"
+          step={0.05}
+          min={props.min}
+          max={props.max}
+          value={value}
+          onChange={event => setValue(event.target.value)}
+          onMouseUp={event => props.setter(round(event.target.value))}
+        />
+      </div>
+    </div>
+  )
+}
+
+function App() {
+  const mounted = useRef(false);
+  const [sepLen, setSepLen] = useState(6.00);
+  const [sepWid, setSepWid] = useState(3.25);
+  const [petLen, setPetLen] = useState(4);
+  const [petWid, setPetWid] = useState(1.25);
+
+  const [label, setLabel] = useState("(loading...)");
+  const [imgSrc, setImgSrc] = useState(logo);
+  const [imgClass, setImgClass] = useState("App-logo");
+
+  useEffect(() => {
+    const runEffect = async () => {
+      const trainCode = await loadModule(trainScript);
+      const predCode = await loadModule(predScript);
+
+      window.languagePluginLoader
+        .then(() => {
+          if (mounted.current === false)
+            return window.pyodide.loadPackage(['numpy', 'pandas', 'scikit-learn']);
+        })
+        .then(() => {
+          const py = window.pyodide;
+
+          if (mounted.current === false) {
+            mounted.current = true;
+            setLabel("(training...)");
+            py.runPython(trainCode);
+          }
+          
+          window.data = {
+            sep_len: sepLen,
+            sep_wid: sepWid,
+            pet_len: petLen,
+            pet_wid: petWid
+          };
+          
+          
+          setLabel("(predicting...)");
+          const predicted = py.runPython(predCode);
+          
+          setLabel(predicted);
+          setImgSrc(urls[predicted]);
+          setImgClass("App-image");
+        })
+    }
+
+    runEffect();
+  }, [sepLen, sepWid, petLen, petWid]);
+
+  return (
+
+    <div className="App">
+      <div className="App-github">
+          <a href="https://github.com/prableen14" target="_blank">
+            <img src={github} height="100%" width="100%" />
+          </a>
+      </div>
+      <div className="App-content">
+        <h2>Iris Classifier (with pyodide and scikit-learn)</h2>
+        <div>
+          <AnnotatedSlider name="Sepal Length" min={4} max={8} value={sepLen} setter={setSepLen} />
+          <AnnotatedSlider name="Sepal Width" min={2} max={4.5} value={sepWid} setter={setSepWid} />
+          <AnnotatedSlider name="Petal Length" min={1} max={7} value={petLen} setter={setPetLen} />
+          <AnnotatedSlider name="Petal Width" min={0} max={2.5} value={petWid} setter={setPetWid} />
+        </div>
+        <div className={imgClass}>
+          <img src={imgSrc} height="100%"></img>
+        </div>
+        <div>Predicted label: {label}</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
